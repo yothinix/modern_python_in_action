b50e66f8ae186f4dd46e64a2cb69424b799bcd04
Yothin Muangsommuk <m@yothinix.com>
2021-05-29 23:37:51 +0700

    Add test for load_db

--- wong/tests/test_reader.py
+++ wong/tests/test_reader.py
@@ -2,7 +2,7 @@ from datetime import date
 
 from ward import test
 
-from ..reader import format_date
+from ..reader import format_date, load_db
 
 
 @test('test_format_date_should_return_date')
@@ -10,3 +10,10 @@ def _() -> None:
     expected = date(2021, 5, 30)
     actual = format_date('May 30, 2021')
     assert expected == actual
+
+
+@test('test load db should return book list')
+def _() -> None:
+    expected = 20
+    actual = load_db()
+    assert expected == len(actual)
