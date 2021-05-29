9f069250197ad859f9243b67d78249fc17863339
Yothin Muangsommuk <m@yothinix.com>
2021-05-29 20:06:52 +0700

    format files

---
+++ wong/wong.py
@@ -0,0 +1,12 @@
+def main():
+    pass
+    # TODO: Query selector
+    #   I'm feeling lucky mode
+    #   call repository class
+    #   - query by name
+    #   - query by author
+    #   - query by pages
+    #   - query by year range
+
+    # TODO: Render class
+    # This should render result in the form of table
