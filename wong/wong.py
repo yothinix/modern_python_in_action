b90c79c1827f5397819bdea0a7a8090f804cf28f
Yothin Muangsommuk <m@yothinix.com>
2021-05-29 21:06:11 +0700

    Remove done todo

--- wong/wong.py
+++ wong/wong.py
@@ -1,10 +1,5 @@
 def main() -> None:
     pass
-    # TODO: Query selector
-    #   I'm feeling lucky mode
-    #   call repository class
-    #   - query by pages
-    #   - query by year range
 
     # TODO: Render class
     # This should render result in the form of table
